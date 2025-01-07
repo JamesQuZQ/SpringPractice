555ec6264e8df26b71b655a2772b450c2e59575f
JamesQuZQ <jamesquzq@gmail.com>
2025-01-07 22:13:48 +1100

    feat: Created spring practice project structure with hello world code test

---
+++ spring-first/src/main/java/org/test/User.java
@@ -0,0 +1,14 @@
+package org.test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class User {
+
+    public void add(){
+        System.out.println("add");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
